d1fdc1966ff4e0a2d3cfe2ee3ab9e72eaf0e5af4
rnovick13 <rnovick13@gmail.com>
2020-07-21 17:57:28 -0400

    refactored to have hero detail component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,6 +8,8 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
+  @Input() hero: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
